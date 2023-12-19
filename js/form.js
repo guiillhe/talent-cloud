3abc47bd685f5ccc3e6a8dc9be68228093d497a2
guiillhe <gja.gja@gmail.com>
2023-12-19 18:58:34 -0300

    ajustes do bruno

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+// Declarações de variáveis
 let inputNome = document.getElementById("nome");
 let inputEmail = document.getElementById("email");
 let inputTelefone = document.getElementById("telefone");
@@ -5,66 +6,73 @@ let selectPlano = document.getElementById("plano");
 let inputMensagem = document.getElementById("mensagem");
 let btnSubmit = document.querySelector('input[type="submit"]');
 
+// Eventos de nome
 inputNome.addEventListener("focus", () => {
     inputNome.style.backgroundColor = "#ee9600";
 });
 
 inputNome.addEventListener("blur", (e) => {    
     if (e.target.value === "") {
-        e.target.style.backgroundColor = "#FF0000";
-        alert("O campo não pode estar vazio.");
-    }else if(!e.target.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').match(/^[a-zA-Z ]+$/)){
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#ffa8b6";
+    }
+    else if (e.target.value.length < 3) {
+        e.target.style.backgroundColor = "#ffa8b6";
+        alert("O campo deve conter no mínimo 3 caracteres.");
+    } 
+    else if(!e.target.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').match(/^[a-zA-Z ]+$/)){
+        e.target.style.backgroundColor = "#ffa8b6";
         alert("O campo deve conter apenas letras.");
-
-    }else if (e.target.value.length < 5) {
-        e.target.style.backgroundColor = "#FF0000";
-        alert("O campo deve conter no mínimo 5 caracteres.");
-    } else if(e.target.value.length > 50) {
-        e.target.style.backgroundColor = "#FF0000";
+    }
+    else if(e.target.value.length > 50) {
+        e.target.style.backgroundColor = "#ffa8b6";
         alert("O campo deve conter no máximo 50 caracteres.");
-        
-    }else {
-        
-        e.target.style.backgroundColor = "#00FF00";
+    }
+    else {
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
+// Eventos de email
 inputEmail.addEventListener("focus", () => {
     inputEmail.style.backgroundColor = "#ee9600";
 });
 
 inputEmail.addEventListener("blur", (e) => {
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const valor = e.target.value.trim();
     if (valor === "") {
-        e.target.style.backgroundColor = "#FF0000";
-        alert("O campo não pode estar vazio.");
-    } else if (!valor.match(regexEmail)) {
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#ffa8b6";
+    } else if (!regexEmail.test(valor)) {
+        e.target.style.backgroundColor = "#ffa8b6";
         alert("Por favor, digite um e-mail válido.");
-    }else{
-        e.target.style.backgroundColor = "#00FF00";
+    } else {
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
+// Eventos de telefone
 inputTelefone.addEventListener("focus", () => {
     inputTelefone.style.backgroundColor = "#ee9600";
 });
 
-inputTelefone.addEventListener("blur", (e) => {      
+inputTelefone.addEventListener("blur", (e) => {
     const valor = inputTelefone.value.trim();
-    if (!valor.match(/^[0-9]+$/)) {        
-        e.target.style.backgroundColor = "#FF0000";
-        e.target.value = ""
-        alert("O campo deve conter apenas números."); 
-    }else {
-        inputTelefone.setCustomValidity("");
-        e.target.style.backgroundColor = "#00FF00";
+
+    if (valor === "") {
+        e.target.style.backgroundColor = "#ffa8b6";
+        return;
     }
-    
+
+    if (!valor.match(/^[0-9]+$/)) {
+        e.target.style.backgroundColor = "#ffa8b6";
+        e.target.value = "";
+        alert("O campo deve conter apenas números.");
+    } else {
+        e.target.style.backgroundColor = "#5ac45a";
+    }
 });
 
+// Eventos de plano
 selectPlano.addEventListener("focus", () => {
     selectPlano.style.backgroundColor = "#ee9600";
 });
@@ -72,32 +80,44 @@ selectPlano.addEventListener("focus", () => {
 selectPlano.addEventListener("blur", (e) => {    
     const valor = document.querySelector("select[name='planos']").value;    
     if (valor === "") {
-        e.target.style.backgroundColor = "#FF0000";        
-        alert("Por favor, selecione um plano.");        
+        e.target.style.backgroundColor = "#ffa8b6";
+        alert("Selecione um plano");
+        e.preventDefault();      
+               
     }else {
-        e.target.style.backgroundColor = "#00FF00";
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
+// Eventos de mensagem
 inputMensagem.addEventListener("focus", () => {
     inputMensagem.style.backgroundColor = "#ee9600";
 });
 
 inputMensagem.addEventListener("blur", (e) => {
-    e.target.style.backgroundColor = "";
+    const valor = e.target.value.trim();
+
+    if (valor === "") {
+        e.target.style.backgroundColor = "#ffa8b6"; 
+    } else {
+        e.target.style.backgroundColor = "";
+    }
 });
 
+// Validações do formulário
+btnSubmit.addEventListener("click", (e) => {
+    const nomeValido = (inputNome.value.trim() !== "" && inputNome.value.length >= 3 && inputNome.value.length <= 50 && /^[a-zA-Z ]+$/.test(inputNome.value));
+    const emailValido = (inputEmail.value.trim() !== "" && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail.value));
+    const telefoneValido = (inputTelefone.value.trim() !== "" && /^[0-9]+$/.test(inputTelefone.value));
+    const planoValido = (document.querySelector("select[name='planos']").value.trim() !== "");
+    const mensagemValida = (document.getElementById("mensagem").value.trim() !== "");
 
-btnSubmit.addEventListener("click", (e) => {    
-    if (inputMensagem.value === "") {        
-        alert("Por favor, preencha todos os campos.");
+    if (!(nomeValido && emailValido && telefoneValido && planoValido && mensagemValida)) {
+        e.preventDefault();
+        alert("Por favor, corrija os erros no formulário antes de enviar.");
     } else {
-        e.preventDefault();         
-        alert("formulário enviado com sucesso!");
+        e.preventDefault();
+        alert("Formulário enviado com sucesso!");
         window.location.href = 'index.html';
     }
-})
-
-
-
-
+});
